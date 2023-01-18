15c38664df17c0df708c5c65a7e4407066ff1331
pho86@my.bcit.ca <pho86@my.bcit.ca>
2023-01-17 21:14:39 -0800

    base functionality and some base styling

---
+++ components/Header/index.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+   return (
+      <nav className="p-4 flex bg-slate-900 w-full justify-center">
+         <ul className="flex 0">
+            <li className="font-bold text-xl text-white">
+               Simple Weather Application
+            </li>
+         </ul>
+      </nav>
+   )
+}
